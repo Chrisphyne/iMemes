07ba00a8ba2f31eaf315a7bad3f4130befcb845d
Chrisphyne <otienoshikuku@gmail.com>
2021-02-17 15:07:36 +0300

    Added Google Billing V1

---
+++ app/src/main/java/imemes/domain/com/imemes_meme_creator/Constants.java
@@ -0,0 +1,17 @@
+package imemes.domain.com.imemes_meme_creator;
+
+/**
+ * Created by delaroy on 11/20/17.
+ */
+
+public class Constants {
+
+    public static final String SKU_IMEMES_MONTHLY = "imemes_monthly";
+//    public static final String SKU_DELAROY_THREEMONTH = "delaroy_threemonth";
+//    public static final String SKU_DELAROY_SIXMONTH = "delaroy_sixmonth";
+//    public static final String SKU_DELAROY_YEARLY = "delaroy_yearly";
+
+    public static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4HR152AZWfgIUECIwfrSI4QRA8mwHv4O3ZsZKUcgWvf7S81xYmv8veDIwxmeQ+NlchPTYHLjuuNmh1kz/WRAL1HWrb9B91NKp7+4FJppubkQJSpwkcOG/y04mur8odRd4QHinZh/NJmDsyR3NeJMibJuGARvDWUPb6gzIFuqM0/4pW7NOEUq4P0NybxTuJsKPyoSfFDXVhsJrroECxPXpwN4y5aNayHUyoa8Y33lIPYD3XOJnxh4b59/rbn81DpqKWVcUcNT9U0HLRSzi4LuBvwni/HsiOfBuCaqCJdU4oJpKmfBnp/eVN4U7P0nCiCOGN8QFBG3NdcHd1GRzuQOGQIDAQAB";
+
+
+}
